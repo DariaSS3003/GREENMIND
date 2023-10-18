8125d322b993ddf485a836fe0064876c4f249f44
Daria <139646443+DariaSS3003@users.noreply.github.com>
2023-10-18 19:52:08 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: "auto",
+    spaceBetween: 48,
+    freeMode: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
